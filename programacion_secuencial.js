b70fcd8a261eb7eaf9cb18c9e77cb3bed1ffd6d9
PerroFrito77 <juan.munera17@gmail.com>
2026-02-25 16:09:51 -0500

    suma

---
+++ programacion_secuencial.js
@@ -0,0 +1,14 @@
+let nombre = "juan";
+let edad = "25";
+let apellido = "munera";
+
+function suma (a,b) {
+    let operacion = a + b;
+    return operacion;
+}
+
+console.log "el resultado de la suma es: " + sumar (numero, edad);
+let Resultadosuma = sumar(numero, edad);
+
+console.log ("nobre completo y apellido " + nombre + "" + apellido + ", edad: "+ edad);
+console.log ("suma de la edad y el numero" + Resultadosuma);
