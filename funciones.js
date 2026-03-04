9df910709e90f8f805a6d158b2cb13b7267e8683
PerroFrito77 <juan.munera17@gmail.com>
2026-03-04 16:14:29 -0500

    funciones puras e inpuras

---
+++ funciones.js
@@ -0,0 +1,50 @@
+function Suma (x) {
+    let accion = x + 5;
+    return accion;
+}
+
+console.log (Suma(7));
+
+
+
+function Division (x) {
+    let accion = x / 7;
+    return accion;
+}
+
+console.log (Division(100));
+
+
+
+let resultado = 0;
+function SumarPrecio (a, b) {
+    return resultado = resultado + a * b;
+
+};
+
+console.log (SumarPrecio (5, 8));
+console.log (SumarPrecio (5, 8));
+console.log (SumarPrecio (5, 8));
+console.log (SumarPrecio (5, 8));
+console.log (SumarPrecio (5, 8));
+console.log (SumarPrecio (5, 8));
+
+
+let palabra = "";
+function Palabra (x) {
+    return palabra = palabra + x;
+};
+
+console.log (Palabra("hola, mundo"));
+console.log (Palabra("otra vez"));
+
+
+function Multiplicar(a) {
+    return a * a;
+}
+
+function SumaDeLaMultiplicacion(num1,num2) {
+    return Multiplicar(num1) + Multiplicar(num2);
+
+};
+console.log(SumaDeLaMultiplicacion(3, 4));
